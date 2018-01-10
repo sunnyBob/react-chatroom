e722d69aab11da9d5a3d409f2e593a0c5cc4bf09
jerry <jerryliu@yunify.com>
2018-01-10 15:47:17 +0800

    add QQ emoji

--- client/src/js/component/chatroom/index.js
+++ client/src/js/component/chatroom/index.js
@@ -1,8 +1,47 @@
 import React from 'react';
-import { Icon } from '../common';
+import { Icon, PopoverManager, Tab, TabItem } from '../common';
 import './chatRoom.less';
 
 class ChatRoom extends React.Component {
+  genEmoji = (count, type = 'QQ') => {
+    const arr = [];
+    const class_name = type === 'QQ' ? 'qemoji' : 'symemoji';
+    for (let i = 0; i <= count; i++) {
+      arr.push(<a className={`${class_name}-${i}`} key={`${type}-${i}`}></a>);
+      if ((i+1) % 15 === 0) {
+        arr.push(<br key={`${type}_${i}`}/>);
+      }
+    }
+    return arr;
+  }
+
+  showEmoji = (e) => {
+    const { pageX, pageY } = e;
+    const emojiArr = [];
+    
+    const content = (
+      <Tab
+        isBoxed={false}
+        handleClick={this.handleToggle}
+        size='small'
+      >
+        <TabItem content="QQ表情">
+          <div className="emojibox">
+            <div className="qq_emoji">
+              {this.genEmoji(104)}
+            </div>
+          </div>
+        </TabItem>
+      </Tab>
+    )
+    PopoverManager.open({
+      x: pageX,
+      y: pageY,
+      reverse: true,
+      content,
+    })
+  }
+
   render() {
     const socket = io("http://127.0.0.1:3000");
     return (
@@ -13,7 +52,7 @@ class ChatRoom extends React.Component {
         </div>
         <div className="wordarea">
           <div className="toolbar is-clearfix">
-            <Icon name="smile-o" size="medium"/>
+            <Icon name="smile-o" size="medium" onClick={this.showEmoji}/>
             <Icon name="folder-o" size="medium"/>
           </div>
           <textarea className="textarea is-primary"></textarea>
